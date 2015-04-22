2ad6d63cc4f9f404199117221d9c213f90ea94dd
Ruei-Min Lin <rueiminl@andrew.cmu.edu>
2015-04-22 15:25:04 -0400

    optimize by store UDPAddr instead of string (need Resolve every time)
    Change-Id: Ifd89a78e24d3364c8fa9a8469f33f25e2cf3bd80

--- src/heartbeat/heartbeat.go
+++ src/heartbeat/heartbeat.go
@@ -23,7 +23,7 @@ func CheckError(err error) {
 
 type Heartbeat struct {
 	from []string // ip:port
-	to []string   // ip:port
+	to []*net.UDPAddr   // ip:port
 	host string
 	socket *net.UDPConn
 	ts map[string]time.Time
@@ -42,12 +42,17 @@ func (this *Heartbeat) Initialize(host string, from []string, to []string) {
 	this.socket, err = net.ListenUDP("udp", addr)
 	CheckError(err)
 	this.from = from
-	this.to = to
+	this.to = make([]*net.UDPAddr, len(to))
+	for i, host := range to {
+		addr, err := net.ResolveUDPAddr("udp", host)
+		CheckError(err)
+		this.to[i] = addr
+	}
 	this.mutex = new(sync.Mutex)
 	this.ts = make(map[string]time.Time)
 	this.dead = make(chan string, DEAD_BUFFER)
-	for _,dst := range this.to {
-		this.ts[dst] = time.Now()
+	for _,src := range this.from {
+		this.ts[src] = time.Now()
 	}
 	go this.RecvFrom()
 	go this.SendTo()
@@ -57,11 +62,9 @@ func (this *Heartbeat) SendTo() {
 	ticker := time.NewTicker(time.Second * HEARTBEAT_TIMEOUT)
 	for t := range ticker.C {
 		// sendout
-		for _, host := range this.to {
-			addr, err := net.ResolveUDPAddr("udp", host)
-			CheckError(err)
+		for _, addr := range this.to {
 			this.socket.WriteToUDP([]byte(this.host), addr)
-			fmt.Println(this.host, "send heartbeat to", host, "at", t)
+			fmt.Println(this.host, "send heartbeat to", addr.String(), "at", t)
 		}
 		
 		// check if dead
